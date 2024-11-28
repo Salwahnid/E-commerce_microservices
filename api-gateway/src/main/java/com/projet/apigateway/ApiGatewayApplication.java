05d589cde6de90b0f7ccb2cb395a66b05ec2be6a
Enami Fatima Zahrae <enamifatimazahrae@gmail.com>
2024-11-28 10:18:07 +0100

    add discover et api-gatway services

--- api-gateway/src/main/java/com/projet/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/projet/apigateway/ApiGatewayApplication.java
@@ -1,15 +1,15 @@
-package com.projet.inventoryservice;
+package com.projet.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
 @EnableDiscoveryClient
-public class InventoryServiceApplication {
+@SpringBootApplication
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(InventoryServiceApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
 }
